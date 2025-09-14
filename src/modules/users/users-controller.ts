3a9b19840f5a6609c01c2d74f0f3a28dab8b84f9
Vitaly Rtishchev <rtivital@gmail.com>
2025-09-14 21:53:35 +0300

    Add session guard example

--- src/modules/users/users-controller.ts
+++ src/modules/users/users-controller.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import { HttpError } from '@/errors';
+import { sessionGuard } from '@/middlewares/session-guard';
 import { prisma } from '@/prisma';
 import { getUserByEmail } from './get-user-by-email';
 
 export const UsersController = express.Router();
 
-UsersController.get('/api/v1/users', async (_req, _res) => {
+UsersController.get('/api/v1/users', sessionGuard, async (_req, _res) => {
   await prisma.user.findMany();
   throw new HttpError(404, 'Not Found', { reason: 'Just a test error' });
 });
 
-UsersController.get('/api/v1/users/email', (req, res) => {
-  const user = getUserByEmail({ email: req.query.email as string });
+UsersController.get('/api/v1/users/email', async (req, res) => {
+  const user = await getUserByEmail({ email: req.query.email as string });
   res.json(user);
 });
