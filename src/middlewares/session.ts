fd10e47f83fbd3be5a74c316cf8ff8ca9602250a
Vitaly Rtishchev <rtivital@gmail.com>
2025-09-22 07:39:13 +0300

    Set seurity cookie settings for production in session middleware

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -6,14 +6,12 @@ import { redisClient } from '@/redis';
 const store: session.Store =
   env.NODE_ENV === 'test'
     ? new session.MemoryStore()
-    : new RedisStore({
-        client: redisClient,
-        prefix: 'session:',
-      });
+    : new RedisStore({ client: redisClient, prefix: 'session:' });
 
 export const sessionMiddleware = session({
   store,
   resave: false,
   saveUninitialized: false,
   secret: env.SESSION_SECRET,
+  cookie: env.NODE_ENV === 'production' ? { secure: true, httpOnly: true } : {},
 });
